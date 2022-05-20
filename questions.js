65e92cde930cb3fa3c519afa03c5c75902913be3
Krazyone96 <70380221+Krazyone96@users.noreply.github.com>
2022-05-20 19:39:41 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,536 @@
+// creating an array and passing the nuimber, questions, options and answers
+
+let data = [
+
+
+	{
+	numb: 1,
+	question: "Adjudicating officer is appointed under __________ of the act",
+	answer: "a. sub-section (I) of section 68",
+	options: [
+		"a. sub-section (I) of section 68",
+		"b. sub-section (II) of section 68",
+		"c. sub-section (I) of section 70",
+		"d. sub-section (II) of section 70",
+		"e.None of the above"
+	]
+	},
+
+	{
+	numb: 2,
+	question: "Apellate Tribunal means Food Safety Apellate Tribunal constituted under ________ of the act",
+	answer: "b. ",
+	options: [
+		"a.",
+		"b.",
+		"c.",
+		"d.",
+		"e.None of the above"
+	]
+	},
+
+	{
+	numb: 3,
+	question: "Authorised Officer means an officer authroized by the food authority under ________ of the act",
+	answer: "c. Section 47",
+	options: [
+		"a. Section 36",
+		"b. Section 68",
+		"c. Section 47",
+		"d. Section 70",
+		"e. None of the above"
+	]
+	},	
+
+	{
+	numb: 4,
+	question: "Inquiry is referred under ________ of the act",
+	answer: "d. Section 68",
+	options: [
+		"a. Section 36",
+		"b. Section 70",
+		"c. Section 43",
+		"d. Section 68",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 5,
+	question: "Authorised officer means an officer authorized by the Food authority under ________ of section 47 of the act",
+	answer: "a. sub-section 5",
+	options: [
+		"a. sub-section 5",
+		"b. sub-section 3",
+		"c. sub-section 7",
+		"d. sub-section 6",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 6,
+	question: "Designated officer is appointed under _______ of the act",
+	answer: "b. Section 36",
+	options: [
+		"a. Section 70",
+		"b. Section 36",
+		"c. Section 43",
+		"d. Section 68",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 7,
+	question: "Notified laboratory means any of the laboratories notified by the food authority under _________ of the act",
+	answer: "c. Section 43",
+	options: [
+		"a. Section 36",
+		"b. Section 68",
+		"c. Section 43",
+		"d. Section 70",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 8,
+	question: "Notified laboratory means any of the laboratories notified by the food authority under _______ of section 43 of the act",
+	answer: "d. Both a and b",
+	options: [
+		"a. sub-section 1",
+		"b. sub-section 2",
+		"c. only a",
+		"d. Both a and b",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 9,
+	question: "Presiding officer means a person appointed as a presiding officer of the Appelate tribunal under ________ of the act",
+	answer: "a. Section 70",
+	options: [
+		"a. Section 70",
+		"b. Section 68",
+		"c. Section 43",
+		"d. Section 36",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 10,
+	question: "________ means any of the laboratories established and/or recognized by the Food authority by notification under sub-section (2) of section 43 of the act",
+	answer: "b. Referral laboratory",
+	options: [
+		"a. Notified laboratory",
+		"b. Referral laboratory",
+		"c. Food laboratory",
+		"d. Both a and b",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 11,
+	question: "The commissioner of food safety shall not be below the rank of __________ to the State Governement",
+	answer: "c. Commissioner and Secretary",
+	options: [
+		"a. Desginated Officer",
+		"b. Sub-divisional Officer",
+		"c. Commissioner and Secretary",
+		"d. District Magistrate",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 12,
+	question: "The powers and duties of Commissioner of Food Safety is provided in the _______ of the Food Safety and Standards Act, 2006",
+	answer: "d. Section 30",
+	options: [
+		"a. Section 70",
+		"b. Section 68",
+		"c. Section 43",
+		"d. Section 30",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 13,
+	question: "The Designated Officer shall not be below the rank of ____________",
+	answer: "a. Sub-divisional officer",
+	options: [
+		"a. Sub-divisional officer",
+		"b. commissioner and secretary",
+		"c. District magistrate",
+		"d. Collector",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 14,
+	question: "Designated officer shall undergo training as specified by the Food Authority, within a period of ________ from the data of appointment",
+	answer: "b. Six months",
+	options: [
+		"a. Three months",
+		"b. Six months",
+		"c. Nine months",
+		"d. One year",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 15,
+	question: "The Designated officer shall function under overall supervision of ____________",
+	answer: "d. Both a and b",
+	options: [
+		"a. Collector",
+		"b. District magistrate",
+		"c. Commissioner of Food Safety",
+		"d. Both a and b",
+		"e. All the above"
+	]
+	},
+
+	{
+	numb: 16,
+	question: "The Designated officer shall ensure the refund of fee for analysis paid by the purchaser as per the provision of ___________ besides the cost of the sample",
+	answer: "c. Section 40",
+	options: [
+		"a. Section 68",
+		"b. Section 36",
+		"c. Section 40",
+		"d. Section 70",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 17,
+	question: "____________ shall ensure the timely disposal of redundant samples, in the manner notified for the seized materials, by the ___________",
+	answer: "d. Designated Officer",
+	options: [
+		"a. Commissioner of Food Safety",
+		"b. Food Safety Officer",
+		"c. Adjudicating Officer",
+		"d. Designated officer",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 18,
+	question: "Designated officer shall ensure timely disposal of redundant samples in the manner notified for the seized materials, by the _______________",
+	answer: "a. Commissioner of Food Safety",
+	options: [
+		"a. Commissioner of Food Safety",
+		"b. Designated Officer",
+		"c. Food Safety Officer",
+		"d. All the above",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 19,
+	question: "The food safety officer shall possess the power conferred by the __________ of the Act",
+	answer: "b. Section 38",
+	options: [
+		"a. Section 43",
+		"b. Section 38",
+		"c. Section 70",
+		"d. Section 68",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 20,
+	question: "________ may cause the person to be examined by a qualified medical professional duly authorized by the Designated Officer",
+	answer: "c. Food Safety Officer",
+	options: [
+		"a. Designated Officer",
+		"b. Food Analyst",
+		"c. Food Safety Officer",
+		"d. Adjudicating Officer",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 21,
+	question: "Food Safety Officer may cause the person to be examined by a qualified medical professional duly authorized by __________",
+	answer: "d. Designated Officer",
+	options: [
+		"a. Commissioner of Food Safety",
+		"b. Food Safety Officer",
+		"c. Food Analyst",
+		"d. Designated Officer",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 22,
+	question: "_______ shall inspect, as frequently as may be prescribed by the Designated Officer, all food establishments licensed for manufacturing, handling, packing on selling of an article of food within the area assigned",
+	answer: "a. Food Safety Officer",
+	options: [
+		"a. Food Safety Officer",
+		"b. Food Analyst",
+		"c. Designated Officer",
+		"d. Commissioner of Food Safety",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 23,
+	question: "_______ shall ensure that the conditions of licenses are being compiled with by each of the FBO carrying on business within the area assigned to him and report to the Designated Officer",
+	answer: "b. Food Safety Officer",
+	options: [
+		"a. Commissioner of Food Safety",
+		"b. Food Safety Officer",
+		"c. Food Analyst",
+		"d. Designated Officer",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 24,
+	question: "________ shall procure and send for analysis if necessary, of samples of any article of food which is in contravention of the provisions of the act",
+	answer: "c. Food Safety Officer",
+	options: [
+		"a. Designated Officer",
+		"b. Food Analyst",
+		"c. Food Safety Officer",
+		"d. Commissioner of Food Safety",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 25,
+	question: "_________ draw samples for purposes of surveillance, survey and research, which shall not be used for prosecution",
+	answer: "d. Food Safety Officer",
+	options: [
+		"a. Designated Officer",
+		"b. Commissioner of Food Safety",
+		"c. Food Analyst",
+		"d. Food Safety Officer",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 26,
+	question: "________ shall investigate any complaint which may be made to him in writing in respect of any contravention of provisions of the act",
+	answer: "a. Food Safety Officer",
+	options: [
+		"a. Food Safety Officer",
+		"b. Food Analyst",
+		"c. Commissioner of Food Safety",
+		"d. Designated Officer",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 27,
+	question: "________ shall maintain a database of all Food Business Operators within the area assigned to him",
+	answer: "b. Food Safety Officer",
+	options: [
+		"a. Food Analyst",
+		"b. Food Safety Officer",
+		"c. Commissioner of Food Safety",
+		"d. Food Business Operator",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 28,
+	question: "_______ recommends the Designated Officer to issue improvement notices to the Food Business Operator, whenever necessary",
+	answer: "c. Food Safety Officer",
+	options: [
+		"a. Designated Officer",
+		"b. Food Analyst",
+		"c. Food Safety Officer",
+		"d. Commissioner of Food Safety",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 29,
+	question: "_____ shall maintain a record of all inspections made and action taken by the Food Safety Officer in the performance of his duties",
+	answer: "d. Food Safety Officer",
+	options: [
+		"a. Commissioner of Food Safety",
+		"b. Designated Officer",
+		"c. Food Analyst",
+		"d. Food Safety Officer",
+		"e. None of the above"
+	]
+	},
+
+
+	{
+	numb: 30,
+	question: "___________ shall make inquries and inspections as maybe necessary to detect the manufacture, storage or sale of articles of food in contravention of the Act or rules framed thereunder",
+	answer: "a. Food Safety Officer",
+	options: [
+		"a. Food Safety Officer",
+		"b. Food Analyst",
+		"c. Designated Officer",
+		"d. Commissioner of Food Safety",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 31,
+	question: "________ shall stop and inspect any vehicle suspected to contain any unsafe food or food which does not comply with the provisions of the act",
+	answer: "b. Food Safety Officer",
+	options: [
+		"a. Food Analyst",
+		"b. Food Safety Officer",
+		"c. Designated Officer",
+		"d. Commissioner of Food Safety",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 32,
+	question: "_________ recommends to the Designated Officer, giving specific grounds, suitable action in regard to licenses issued to any F.B.O, if on inspection the Food Safety Officer finds that the F.B.O had violated the conditions for grant of license",
+	answer: "c. Food Safety Officer",
+	options: [
+		"a. Designated Officer",
+		"b. Food Analyst",
+		"c. Food Safety Officer",
+		"d. Commissioner of Food Safety",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 33,
+	question: "________ shall carry out food safety surveillance to identify and address the safety hazards",
+	answer: "d. Food Safety Officer",
+	options: [
+		"a. Commissioner of Food Safety",
+		"b. Food Analyst",
+		"c. Designated Officer",
+		"d. Food Safety Officer",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 34,
+	question: "______ responds to incidents of food poisoning in his area and to send report to and ask the Designated Officer to enable him to initiate corrective action",
+	answer: "a. Food Safety Officer",
+	options: [
+		"a. Food Safety Officer",
+		"b. Designatd Officer",
+		"c. Food Analyst",
+		"d. Commissioner of Food Safety",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 35,
+	question: "_________ facilitates preparation of Food Safety plans for Panchayat and Municipalities",
+	answer: "b. Food Safety Officer",
+	options: [
+		"a. Designated Officer",
+		"b. Food Safety Officer",
+		"c. Food Analyst",
+		"d. Commissioner of Food Safety",
+		"e. None of the above"
+	]
+	},
+
+
+	{
+	numb: 36,
+	question: "The parameter and guidelines for preparation of Food Safety Plans for Panchayats and Municipalities are given in __________ of regulations",
+	answer: "c. Schedule IV of Chapter 3",
+	options: [
+		"a. Schedule III of Chapter 3",
+		"b. Schedule IV of Chapter 2",
+		"c. Schedule IV of Chapter 3",
+		"d. Schedule III of Chapter 2",
+		"e. None of the above"
+	]
+	},
+
+
+	{
+	numb: 37,
+	question: "___________ shall detain imported packages which are respected to contain articles of food, the import or sale of which is prohibited",
+	answer: "d. Food Safety Officer",
+	options: [
+		"a. Commissioner of Food Safety",
+		"b. Food Analyst",
+		"c. Designated Officer",
+		"d. Food Safety Officer",
+		"e. None of the above"
+	]
+	},
+
+	{
+	numb: 38,
+	question: "_____________ shall coordinate with the F.B.Os in the area of operation and introduction of food safety systems to the F.B.O",
+	answer: "a. Food Safety Officer",
+	options: [
+		"a. Food Safety Officer",
+		"b. Designated Officer",
+		"c. Food Analyst",
+		"d. Commissioner of Food Safety",
+		"e. None of the above"
+	]
+	},
+
+
+	{
+	numb: 39,
+	question: "After completion of analysis of article of food, the Food Analyst shall send his report to the Designated Officer, the purchaser of the article of food in ___________",
+	answer: "b. Form VII A",
+	options: [
+		"a. Form VI A",
+		"b. Form VII A",
+		"c. Form V A",
+		"d. Form VIII A",
+		"e. None of the above"
+	]
+	},
+
+
+];
+
+
+for (let i = data.length - 1; i>0; i--){
+	let j = Math.floor(Math.random() * i)
+	let k = data[i]
+	data[i] = data[j]
+	data[j] = k
+	}
+
+let questions =[]
+
+
+for (let a = questions.length - 1; a>0; a--){
+	let b = Math.floor(Math.random()*a)
+	let c = questions[a]
+	questions[a] = questions[b]
+	questions[b] = c
+	}
